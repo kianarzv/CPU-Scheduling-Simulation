add9b24cd7bd0cc38d24d9d6ac80915f272c1c3d
kianarzv <kiana.razavi1383@gmail.com>
2025-06-02 13:06:15 +0330

    implementing FCFS

--- scheduler.c
+++ scheduler.c
@@ -1,18 +1,36 @@
 #include "scheduler.h"
+#include <stdlib.h>
+#include <string.h>
 
-// ---------------- Scheduling Algorithms ----------------
-
-// FCFS Scheduling
+// FCFS 
 Metrics fcfs_metrics(Process proc[], int n) {
-    //TODO
+    Process *p = malloc(n * sizeof(Process));
+    memcpy(p, proc, n * sizeof(Process));
+    int time = 0;
+    float totalTurnaround = 0, totalWaiting = 0, totalResponse = 0;
+    for (int i = 0; i < n; i++) {
+        if (time < p[i].arrivalTime) {
+            time = p[i].arrivalTime;
+        }
+        p[i].startTime = time;
+        time += p[i].burstTime;
+        p[i].completionTime = time;
+
+        int turnaround = p[i].completionTime - p[i].arrivalTime;
+        int waiting = turnaround - p[i].burstTime;
+        int response = p[i].startTime - p[i].arrivalTime;
+
+        totalTurnaround += turnaround;
+        totalWaiting += waiting;
+        totalResponse += response;
+    }
+
+    free(p);
+    Metrics m = {
+        totalTurnaround / n,
+        totalWaiting / n,
+        totalResponse / n
+    };
+    return m;
 }
 
-// SJF Scheduling (Non-preemptive)
-Metrics sjf_metrics(Process proc[], int n) {
-    //TODO
-}
-
-// Round Robin Scheduling (Revised)
-Metrics rr_metrics(Process proc[], int n, int timeQuantum) {
-    //TODO
-}
